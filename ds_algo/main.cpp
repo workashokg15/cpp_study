2e4a674757b8bcac50e9b97b6909c09764bd6f42
Ashok Gopalakrishnan <workashok.g15@gmail.com>
2025-01-08 08:35:40 +0530

    smartpointer_unique_ptr

--- ds_algo/main.cpp
+++ ds_algo/main.cpp
@@ -2,10 +2,12 @@
 #include "smartptr.hpp"
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 using namespace common::ds_algo;
 
+//Using own implementation of smartpointer to check destructor called after out of scope
 void scopetester()
 {
     SmartPtr<BST<int>> bst_tree1(new BST<int>());
@@ -42,6 +44,44 @@ void scopetester()
     }
 }
 
+//Using own implementation of smartpointer to check destructor called after out of scope
+void scopetester_using_unique_ptr()
+{
+    unique_ptr<BST<int>> bst_tree1(new BST<int>());
+    //BST<int> bst_tree1;
+    bst_tree1->insert_node(new BSTNode<int>{5}); 
+    bst_tree1->insert_node(new BSTNode<int>{3});
+    bst_tree1->insert_node(new BSTNode<int>{2});
+    bst_tree1->insert_node(new BSTNode<int>{4});
+    bst_tree1->insert_node(new BSTNode<int>{10});
+    bst_tree1->insert_node(new BSTNode<int>{8});
+    bst_tree1->insert_node(new BSTNode<int>{6});
+    bst_tree1->insert_node(new BSTNode<int>{12});
+    bst_tree1->insert_node(new BSTNode<int>{14});
+
+    print_bst_pre_order(*bst_tree1);
+    int key = 8;
+
+    cout << "searching key " << key << endl; 
+    BSTNode<int>* node = bst_tree1->search_key(key);
+    if (node){
+        cout << "Node for key "<< key <<" found at : " << node;
+    } else {
+        cout << "Search failed for key : " << key << endl;
+    }
+
+    key = 7;
+
+    cout << "searching key " << key << endl; 
+    node = bst_tree1->search_key(key);
+    if (node){
+        cout << "Node for key "<< key <<" found at : " << node;
+    } else {
+        cout << "Search failed for key : " << key << endl;
+    }
+}
+
+
 int main()
 {
     cout << "BST impl example" << endl;
@@ -69,5 +109,9 @@ int main()
     name_tree->delete_node(name_del_key);
     print_bst_pre_order(*name_tree);   
  
+    cout << "===>>>Test using unique_ptr" << endl;
+    scopetester_using_unique_ptr();
+    cout << "<<<===Test using unique_ptr" << endl;
+
     return 0;
 }
