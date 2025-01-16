4e40f12fcec6b7d115a1e04ce52cb6ddc9d15163
Ashok Gopalakrishnan <workashok.g15@gmail.com>
2025-01-16 12:55:29 +0530

    adding

---
+++ dp/creational/builder/builder.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace AshokProgramming{
+    namespace dp {
+        namespace creational{
+            class builder {
+
+            };
+        }//namespace creational
+    }//namespace dp
+}//namespace AshokProgramming
+
---
+++ dp/creational/builder/main.cpp
@@ -0,0 +1,64 @@
+#include "builder.hpp"
+
+// C++ Program to Implement Reflection Using Run Time Type
+// Information (RTTI)
+
+#include <cxxabi.h>
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+
+// Base class with a virtual destructor
+class Base {
+public:
+    virtual ~Base() {}
+};
+
+// Derived class inheriting from Base
+class Derived : public Base {
+};
+
+void check_rtti()
+{
+  // Creating a Derived object through Base pointer
+    Base* basePtr
+        = new Base(); 
+                       
+
+    // Get type information of the object pointed to by
+    // basePtr
+    const type_info& typeInfo = typeid(*basePtr);
+    cout << "Type name: " << typeInfo.name() << endl;
+
+    // Demangle the type name (if needed)
+    int status;
+    char* demangled = abi::__cxa_demangle(
+        typeInfo.name(), nullptr, nullptr, &status);
+    string typeName
+        = (status == 0) ? demangled : typeInfo.name();
+    cout << "Demangled type name: " << typeName << endl;
+    free(demangled);
+
+    // Compare types using typeid
+    if (typeid(*basePtr) == typeid(Derived)) {
+        cout << "Object is of type Derived" << endl;
+    }
+
+    // Safe casting using dynamic_cast
+    Derived* derivedPtr = dynamic_cast<Derived*>(basePtr);
+    if (derivedPtr) {
+        cout << "Successfully cast to Derived" << endl;
+    }
+    else {
+        cout << "Casting failed" << endl;
+    }
+  // Freeing the allocated memory
+
+    delete basePtr; 
+}
+
+int main()
+{
+    check_rtti();
+}
